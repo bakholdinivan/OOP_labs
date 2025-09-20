39ea43bad549f454413289d457702716a24f6c0e
Ivan Bakholdin <bakholdin.ivan11@gmail.com>
2025-09-20 17:57:19 +0300

    Delete tasks/lab1/build directory

--- tasks/lab1/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_OOP/tasks/lab1
-# Build directory: /workspaces/labs_OOP/tasks/lab1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(BoxCapacityTests "/workspaces/labs_OOP/tasks/lab1/build/tests")
-set_tests_properties(BoxCapacityTests PROPERTIES  _BACKTRACE_TRIPLES "/workspaces/labs_OOP/tasks/lab1/CMakeLists.txt;37;add_test;/workspaces/labs_OOP/tasks/lab1/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- tasks/lab1/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/labs_OOP/tasks/lab1/build/_deps/googletest-src
-# Build directory: /workspaces/labs_OOP/tasks/lab1/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
